0f9dfcc256cb78e3319580fd1b0d1fa76ae8cba9
Xader726 <42283529+Xader726@users.noreply.github.com>
2018-09-22 03:27:43 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,17 +1,18 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+
+$name = $_FILES["myfile"]["name"];
+$type = $_FILES["myfile"]["type"];
+$size = $_FILES["myfile"]["size"];
+$temp = $_FILES["myfile"]["tmp_name"];
+$error = $_FILES["myfile"]["error"];
+
+if($size > 1000000)
+{
+	die("The file is too big!");
 }
+else	
+move_uploaded_file($temp,"uploaded/".$name);
+
+header("location: uploadcomplete.html");
+
 ?>
